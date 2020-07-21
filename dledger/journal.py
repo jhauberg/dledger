4267b976443d7f7d8f1e6c4324bffd115b866030
Jacob Hauberg Hansen <jacob.hauberg@gmail.com>
2020-07-21 18:21:50 +0200

    Allow only a single whitespace between symbol and amount
    Typically, any whitespace in excess of one is used *only* for formatting/aligning in the journal, and is not expected to be part of the displayed output.

--- dledger/journal.py
+++ dledger/journal.py
@@ -456,6 +456,12 @@ def parse_amount(amount: str, *, location: Tuple[str, int]) \
         rhs = c + rhs
     # assume first part of string the amount and remainder the symbol
     amount = amount[:len(amount) - len(rhs)]
+    # trim trailing whitespace; leading whitespace considered intentional
+    rhs = rhs.rstrip()
+    # allow up to one leading whitespace
+    rhs = (rhs[len(rhs) - len(rhs.lstrip()) - 1:]
+           if len(rhs) > len(rhs.lstrip()) else
+           rhs)
     # accumulate left-hand side of string by going through each character
     lhs = ''
     for c in amount:
@@ -464,6 +470,12 @@ def parse_amount(amount: str, *, location: Tuple[str, int]) \
         lhs += c
     # assume remainder of string is the amount and lhs is the symbol
     amount = amount[len(lhs):]
+    # trim leading whitespace; trailing whitespace considered intentional
+    lhs = lhs.lstrip()
+    # allow up to one trailing whitespace
+    lhs = (lhs[:len(lhs.rstrip()) + 1]
+           if len(lhs) > len(lhs.rstrip()) else
+           lhs)
     # parse out symbol using left/right-hand sides of the string
     if len(rhs) > 0:
         symbol = rhs.strip()
