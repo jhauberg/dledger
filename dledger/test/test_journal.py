df05698420eff9cb4146c81ca3e4dd27b72d1644
Jacob Hauberg Hansen <jacob.hauberg@gmail.com>
2021-01-19 11:14:37 +0100

    Add test

--- dledger/test/test_journal.py
+++ dledger/test/test_journal.py
@@ -1058,6 +1058,89 @@ def test_include_journal():
     )
 
 
+def test_include_journal_out_of_order():
+    trysetlocale(locale.LC_NUMERIC, ["en_US", "en-US", "en"])
+
+    path = "../example/include_out_of_order.journal"
+
+    included_resolved_path = "../example/simple.journal"
+
+    records = read(path, kind="journal")
+
+    assert len(records) == 7
+
+    assert records[0] == Transaction(
+        date(2019, 2, 14),
+        "AAPL",
+        100,
+        amount=Amount(73, places=0, symbol="$", fmt="$ %s"),
+        dividend=Amount(0.73, places=2, symbol="$", fmt="$ %s"),
+        entry_attr=EntryAttributes(
+            location=(included_resolved_path, 3), positioning=(100, POSITION_SET)
+        ),
+    )
+    assert records[1] == Transaction(
+        date(2019, 5, 16),
+        "AAPL",
+        100,
+        amount=Amount(77, places=0, symbol="$", fmt="$ %s"),
+        dividend=Amount(0.77, places=2, symbol="$", fmt="$ %s"),
+        entry_attr=EntryAttributes(
+            location=(included_resolved_path, 6), positioning=(None, POSITION_SET)
+        ),
+    )
+    assert records[2] == Transaction(
+        date(2019, 8, 15),
+        "AAPL",
+        100,
+        amount=Amount(77, places=0, symbol="$", fmt="$ %s"),
+        dividend=Amount(0.77, places=2, symbol="$", fmt="$ %s"),
+        entry_attr=EntryAttributes(
+            location=(included_resolved_path, 9), positioning=(None, POSITION_SET)
+        ),
+    )
+    assert records[3] == Transaction(
+        date(2019, 11, 14),
+        "AAPL",
+        100,
+        amount=Amount(77, places=0, symbol="$", fmt="$ %s"),
+        dividend=Amount(0.77, places=2, symbol="$", fmt="$ %s"),
+        entry_attr=EntryAttributes(
+            location=(included_resolved_path, 12), positioning=(None, POSITION_SET)
+        ),
+    )
+    assert records[4] == Transaction(
+        date(2020, 2, 13),
+        "AAPL",
+        100,
+        amount=Amount(77, places=0, symbol="$", fmt="$ %s"),
+        dividend=Amount(0.77, places=2, symbol="$", fmt="$ %s"),
+        entry_attr=EntryAttributes(
+            location=(path, 5), positioning=(None, POSITION_SET)
+        ),
+    )
+    assert records[5] == Transaction(
+        date(2020, 5, 15),
+        "AAPL",
+        100,
+        amount=Amount(82, places=0, symbol="$", fmt="$ %s"),
+        dividend=Amount(0.82, places=2, symbol="$", fmt="$ %s"),
+        entry_attr=EntryAttributes(
+            location=(path, 8), positioning=(None, POSITION_SET)
+        ),
+    )
+    assert records[5] == Transaction(
+        date(2020, 8, 14),
+        "AAPL",
+        100,
+        amount=Amount(82, places=0, symbol="$", fmt="$ %s"),
+        dividend=Amount(0.82, places=2, symbol="$", fmt="$ %s"),
+        entry_attr=EntryAttributes(
+            location=(path, 11), positioning=(None, POSITION_SET)
+        ),
+    )
+
+
 def test_write():
     trysetlocale(locale.LC_NUMERIC, ["en_US", "en-US", "en"])
 
