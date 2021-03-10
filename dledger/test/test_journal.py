7ba4a008fec4ee6b29b718428dfaa6930c9b2fdf
Jacob Hauberg Hansen <jacob.hauberg@gmail.com>
2021-03-10 15:37:04 +0100

    Add split tests
    See #30

--- dledger/test/test_journal.py
+++ dledger/test/test_journal.py
@@ -9,6 +9,10 @@ from dledger.journal import (
     Distribution,
     ParseError,
     POSITION_SET,
+    POSITION_ADD,
+    POSITION_SUB,
+    POSITION_SPLIT,
+    POSITION_SPLIT_WHOLE,
     read,
     excluding_redundant_transactions,
     parse_amount,
@@ -18,6 +22,7 @@ from dledger.projection import (
     GeneratedAmount,
     scheduled_transactions,
     convert_estimates,
+    adjusting_for_splits,
 )
 from dledger.localeutil import trysetlocale
 from dledger.formatutil import decimalplaces
@@ -382,6 +387,15 @@ def test_positions_journal():
     records = read(path, kind="journal")
 
     assert len(records) == 5
+
+    assert records[2] == Transaction(
+        date(2019, 6, 4),
+        "AAPL",
+        120,
+        entry_attr=EntryAttributes(
+            location=(path, 11), positioning=(20, POSITION_ADD)
+        ),
+    )
 
     records = excluding_redundant_transactions(records)
 
@@ -1256,3 +1270,336 @@ def test_nordnet_import_ambiguity():
     else:
         assert False
 
+
+def test_splits_whole():
+    trysetlocale(locale.LC_NUMERIC, ["en_US", "en-US", "en"])
+
+    path = "../example/split.journal"
+
+    records = read(path, kind="journal")
+
+    assert len(records) == 4
+
+    assert records[0] == Transaction(
+        date(2021, 1, 1),
+        "ABC",
+        10,
+        amount=Amount(1, places=0, symbol="$", fmt="$ %s"),
+        dividend=Amount(0.1, places=1, symbol="$", fmt="$ %s"),
+        entry_attr=EntryAttributes(
+            location=(path, 3), positioning=(10, POSITION_SET)
+        ),
+    )
+    assert records[1] == Transaction(
+        date(2021, 2, 1),
+        "ABC",
+        20,
+        entry_attr=EntryAttributes(
+            location=(path, 6), positioning=(10, POSITION_ADD)
+        ),
+    )
+    assert records[2] == Transaction(
+        date(2021, 2, 10),
+        "ABC",
+        40,
+        entry_attr=EntryAttributes(
+            location=(path, 8), positioning=(2, POSITION_SPLIT_WHOLE)
+        ),
+    )
+    assert records[3] == Transaction(
+        date(2021, 4, 1),
+        "ABC",
+        40,
+        amount=Amount(2, places=0, symbol="$", fmt="$ %s"),
+        dividend=Amount(0.05, places=2, symbol="$", fmt="$ %s"),
+        entry_attr=EntryAttributes(
+            location=(path, 10), positioning=(None, POSITION_SET)
+        ),
+    )
+
+
+def test_splits_fractional():
+    trysetlocale(locale.LC_NUMERIC, ["en_US", "en-US", "en"])
+
+    path = "../example/split_fractional.journal"
+
+    records = read(path, kind="journal")
+
+    assert len(records) == 7
+
+    assert records[0] == Transaction(
+        date(2021, 1, 1),
+        "ABC",
+        10,
+        amount=Amount(1, places=0, symbol="$", fmt="$ %s"),
+        dividend=Amount(0.1, places=1, symbol="$", fmt="$ %s"),
+        entry_attr=EntryAttributes(
+            location=(path, 3), positioning=(10, POSITION_SET)
+        ),
+    )
+    assert records[1] == Transaction(
+        date(2021, 2, 1),
+        "ABC",
+        20,
+        entry_attr=EntryAttributes(
+            location=(path, 6), positioning=(10, POSITION_ADD)
+        ),
+    )
+    assert records[2] == Transaction(
+        date(2021, 2, 10),
+        "ABC",
+        40,
+        entry_attr=EntryAttributes(
+            location=(path, 8), positioning=(2, POSITION_SPLIT_WHOLE)
+        ),
+    )
+    assert records[3] == Transaction(
+        date(2021, 2, 11),
+        "ABC",
+        39,
+        entry_attr=EntryAttributes(
+            location=(path, 10), positioning=(1, POSITION_SUB)
+        ),
+    )
+    assert records[4] == Transaction(
+        date(2021, 4, 1),
+        "ABC",
+        39,
+        amount=Amount(1.95, places=2, symbol="$", fmt="$ %s"),
+        dividend=Amount(0.05, places=2, symbol="$", fmt="$ %s"),
+        entry_attr=EntryAttributes(
+            location=(path, 12), positioning=(None, POSITION_SET)
+        ),
+    )
+    assert records[5] == Transaction(
+        date(2021, 5, 10),
+        "ABC",
+        58.5,
+        entry_attr=EntryAttributes(
+            location=(path, 15), positioning=(1.5, POSITION_SPLIT)
+        ),
+    )
+    assert records[6] == Transaction(
+        date(2021, 7, 1),
+        "ABC",
+        58.5,
+        amount=Amount(1.95, places=2, symbol="$", fmt="$ %s"),
+        dividend=Amount(0.0333, places=4, symbol="$", fmt="$ %s"),
+        entry_attr=EntryAttributes(
+            location=(path, 17), positioning=(None, POSITION_SET)
+        ),
+    )
+
+
+def test_reverse_split():
+    trysetlocale(locale.LC_NUMERIC, ["en_US", "en-US", "en"])
+
+    path = "../example/split_reverse.journal"
+
+    records = read(path, kind="journal")
+
+    assert len(records) == 4
+
+    assert records[0] == Transaction(
+        date(2021, 1, 1),
+        "ABC",
+        10,
+        amount=Amount(1, places=0, symbol="$", fmt="$ %s"),
+        dividend=Amount(0.1, places=1, symbol="$", fmt="$ %s"),
+        entry_attr=EntryAttributes(
+            location=(path, 3), positioning=(10, POSITION_SET)
+        ),
+    )
+    assert records[1] == Transaction(
+        date(2021, 2, 1),
+        "ABC",
+        20,
+        entry_attr=EntryAttributes(
+            location=(path, 6), positioning=(10, POSITION_ADD)
+        ),
+    )
+    assert records[2] == Transaction(
+        date(2021, 2, 10),
+        "ABC",
+        10,
+        entry_attr=EntryAttributes(
+            location=(path, 8), positioning=(0.5, POSITION_SPLIT_WHOLE)
+        ),
+    )
+    assert records[3] == Transaction(
+        date(2021, 4, 1),
+        "ABC",
+        10,
+        amount=Amount(2, places=0, symbol="$", fmt="$ %s"),
+        dividend=Amount(0.2, places=1, symbol="$", fmt="$ %s"),
+        entry_attr=EntryAttributes(
+            location=(path, 10), positioning=(None, POSITION_SET)
+        ),
+    )
+
+
+def test_adjusting_for_splits_whole():
+    trysetlocale(locale.LC_NUMERIC, ["en_US", "en-US", "en"])
+
+    path = "../example/split.journal"
+
+    records = adjusting_for_splits(read(path, kind="journal"))
+
+    assert len(records) == 4
+
+    assert records[0] == Transaction(
+        date(2021, 1, 1),
+        "ABC",
+        20,
+        amount=Amount(1, places=0, symbol="$", fmt="$ %s"),
+        dividend=Amount(0.05, places=2, symbol="$", fmt="$ %s"),
+        entry_attr=EntryAttributes(
+            location=(path, 3), positioning=(10, POSITION_SET)
+        ),
+    )
+    assert records[1] == Transaction(
+        date(2021, 2, 1),
+        "ABC",
+        40,
+        entry_attr=EntryAttributes(
+            location=(path, 6), positioning=(10, POSITION_ADD)
+        ),
+    )
+    assert records[2] == Transaction(
+        date(2021, 2, 10),
+        "ABC",
+        40,
+        entry_attr=EntryAttributes(
+            location=(path, 8), positioning=(2, POSITION_SPLIT_WHOLE)
+        ),
+    )
+    assert records[3] == Transaction(
+        date(2021, 4, 1),
+        "ABC",
+        40,
+        amount=Amount(2, places=0, symbol="$", fmt="$ %s"),
+        dividend=Amount(0.05, places=2, symbol="$", fmt="$ %s"),
+        entry_attr=EntryAttributes(
+            location=(path, 10), positioning=(None, POSITION_SET)
+        ),
+    )
+
+
+def test_adjusting_for_splits_fractional():
+    trysetlocale(locale.LC_NUMERIC, ["en_US", "en-US", "en"])
+
+    path = "../example/split_fractional.journal"
+
+    records = adjusting_for_splits(read(path, kind="journal"))
+
+    assert len(records) == 7
+
+    assert records[0] == Transaction(
+        date(2021, 1, 1),
+        "ABC",
+        30,
+        amount=Amount(1, places=0, symbol="$", fmt="$ %s"),
+        dividend=Amount(0.0333, places=4, symbol="$", fmt="$ %s"),
+        entry_attr=EntryAttributes(
+            location=(path, 3), positioning=(10, POSITION_SET)
+        ),
+    )
+    assert records[1] == Transaction(
+        date(2021, 2, 1),
+        "ABC",
+        60,
+        entry_attr=EntryAttributes(
+            location=(path, 6), positioning=(10, POSITION_ADD)
+        ),
+    )
+    assert records[2] == Transaction(
+        date(2021, 2, 10),
+        "ABC",
+        60,
+        entry_attr=EntryAttributes(
+            location=(path, 8), positioning=(2, POSITION_SPLIT_WHOLE)
+        ),
+    )
+    assert records[3] == Transaction(
+        date(2021, 2, 11),
+        "ABC",
+        58.5,
+        entry_attr=EntryAttributes(
+            location=(path, 10), positioning=(1, POSITION_SUB)
+        ),
+    )
+    assert records[4] == Transaction(
+        date(2021, 4, 1),
+        "ABC",
+        58.5,
+        amount=Amount(1.95, places=2, symbol="$", fmt="$ %s"),
+        dividend=Amount(0.0333, places=4, symbol="$", fmt="$ %s"),
+        entry_attr=EntryAttributes(
+            location=(path, 12), positioning=(None, POSITION_SET)
+        ),
+    )
+    assert records[5] == Transaction(
+        date(2021, 5, 10),
+        "ABC",
+        58.5,
+        entry_attr=EntryAttributes(
+            location=(path, 15), positioning=(1.5, POSITION_SPLIT)
+        ),
+    )
+    assert records[6] == Transaction(
+        date(2021, 7, 1),
+        "ABC",
+        58.5,
+        amount=Amount(1.95, places=2, symbol="$", fmt="$ %s"),
+        dividend=Amount(0.0333, places=4, symbol="$", fmt="$ %s"),
+        entry_attr=EntryAttributes(
+            location=(path, 17), positioning=(None, POSITION_SET)
+        ),
+    )
+
+
+def test_adjusting_for_splits_reverse():
+    trysetlocale(locale.LC_NUMERIC, ["en_US", "en-US", "en"])
+
+    path = "../example/split_reverse.journal"
+
+    records = adjusting_for_splits(read(path, kind="journal"))
+
+    assert len(records) == 4
+
+    assert records[0] == Transaction(
+        date(2021, 1, 1),
+        "ABC",
+        5,
+        amount=Amount(1, places=0, symbol="$", fmt="$ %s"),
+        dividend=Amount(0.2, places=1, symbol="$", fmt="$ %s"),
+        entry_attr=EntryAttributes(
+            location=(path, 3), positioning=(10, POSITION_SET)
+        ),
+    )
+    assert records[1] == Transaction(
+        date(2021, 2, 1),
+        "ABC",
+        10,
+        entry_attr=EntryAttributes(
+            location=(path, 6), positioning=(10, POSITION_ADD)
+        ),
+    )
+    assert records[2] == Transaction(
+        date(2021, 2, 10),
+        "ABC",
+        10,
+        entry_attr=EntryAttributes(
+            location=(path, 8), positioning=(0.5, POSITION_SPLIT_WHOLE)
+        ),
+    )
+    assert records[3] == Transaction(
+        date(2021, 4, 1),
+        "ABC",
+        10,
+        amount=Amount(2, places=0, symbol="$", fmt="$ %s"),
+        dividend=Amount(0.2, places=1, symbol="$", fmt="$ %s"),
+        entry_attr=EntryAttributes(
+            location=(path, 10), positioning=(None, POSITION_SET)
+        ),
+    )
