fff34076dd81a3a283e71d171f8a71b0e943cbcc
Jacob Hauberg Hansen <jacob.hauberg@gmail.com>
2021-04-15 14:37:15 +0200

    Add test

---
+++ test/test_locale.py
@@ -0,0 +1,24 @@
+import locale
+
+from dledger.localeutil import (
+    tempconv,
+    DECIMAL_POINT_PERIOD,
+    DECIMAL_POINT_COMMA,
+)
+
+
+def test_localeconv_override():
+    previous_decimal_point = locale.localeconv()["decimal_point"]
+
+    assert (previous_decimal_point == "." or
+            previous_decimal_point == ",")
+
+    with tempconv(DECIMAL_POINT_PERIOD):
+        assert locale.localeconv()["decimal_point"] == "."
+
+    assert locale.localeconv()["decimal_point"] == previous_decimal_point
+
+    with tempconv(DECIMAL_POINT_COMMA):
+        assert locale.localeconv()["decimal_point"] == ","
+
+    assert locale.localeconv()["decimal_point"] == previous_decimal_point
